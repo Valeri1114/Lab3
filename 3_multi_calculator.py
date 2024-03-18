b3ccd3630e6d14cf1004cb7ccdd68d00d28cd518
Valeri1114 <Valeri.tut.i.tam@gmail.com>
2024-03-18 12:20:12 +0800

    change

--- 3_multi_calculator.py
+++ 3_multi_calculator.py
@@ -1,3 +1,5 @@
+# Valeriia Pasichna
+# G12460782 
 
 Sum = 0
 
@@ -9,9 +11,9 @@ op = input ("Enter operator: ")
 
 # 2. Process
 if op == "+":
-    sum == int(x1) + int (x2)
+    sum = int(x1) + int (x2)
 elif op == "-":
-    sum == int(x1) - int(x2)
+    sum = int(x1) - int(x2)
 elif op == "*":
     sum = int(x1) * int(x2)
 elif op == "/":
