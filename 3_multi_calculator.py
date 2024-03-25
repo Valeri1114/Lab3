6d1f31511c1307c1375a88a91919f02d854a42ea
Valeri1114 <Valeri.tut.i.tam@gmail.com>
2024-03-25 10:41:46 +0800

    Change

--- 3_multi_calculator.py
+++ 3_multi_calculator.py
@@ -1,7 +1,7 @@
 # Valeriia Pasichna
 # G12460782 
 
-Sum = 0
+sum = 0
 
 # 1. input
 
@@ -10,6 +10,7 @@ x2= input("Type x2: ")
 op = input ("Enter operator: ")
 
 # 2. Process
+
 if op == "+":
     sum = int(x1) + int (x2)
 elif op == "-":
